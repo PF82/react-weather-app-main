4bd28c1c66df6b8a7461971151a58e2aee5d0cd2
PF82 <pedrotcfernandes@gmail.com>
2022-10-15 18:52:41 +0100

    react-weather-app-main

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import UilReact from '@iconscout/react-unicons/icons/uil-react'
+
+function App() {
+  return (
+    <div className="mx-auto">
+
+    </div>
+  );
+}
+
+export default App;
