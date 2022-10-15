8d7dbf394402e7e4b624e94a14e4228551cf9346
PF82 <pedrotcfernandes@gmail.com>
2022-10-15 20:00:37 +0100

    rev-01 wip

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import UilReact from '@iconscout/react-unicons/icons/uil-react'
+import TopButtons from './components/TopButtons';
+import Inputs from './components/Inputs';
 
 function App() {
-  return (
-    <div className="mx-auto">
 
+  return (
+    <div className="container">
+      <TopButtons />
+      <Inputs />
     </div>
   );
 }
---
+++ src/components/TopButtons.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+
+function TopButtons() {
+
+  const cities = [
+    {
+      id: 1,
+      title: 'London'
+    },
+    {
+      id: 2,
+      title: 'Sydney'
+    },
+    {
+      id: 3,
+      title: 'Tokyo'
+    },
+    {
+      id: 4,
+      title: 'Toronto'
+    },
+    {
+      id: 5,
+      title: 'Paris'
+    },
+  ]
+
+  return (
+    <div className='top-buttons'>
+      {cities.map((city) => (
+        <button key={city.id} className='button'>{city.title}</button>
+      ))}
+    </div>
+  );
+}
+
+export default TopButtons
