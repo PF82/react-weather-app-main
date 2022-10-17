69967e9fda1c8afc15cb816b9bf6b084f8d1a9c2
PF82 <pedrotcfernandes@gmail.com>
2022-10-17 03:13:29 +0100

    rev-06 wip (1:32:00)

--- src/services/weatherService.js
+++ src/services/weatherService.js
@@ -1,3 +1,5 @@
+import { DateTime } from "luxon";
+
 // react-weather-app-m openweather API key
 const API_KEY = "9c61d7516f674d61ce67583b5f1d3f23";
 const BASE_URL = "https://api.openweathermap.org/data/2.5";
@@ -30,11 +32,43 @@ const formatCurrentWeather = (data) => {
     return { lat, lon, temp, feels_like, temp_min, temp_max, humidity, name, dt, country, sunrise, sunset, details, icon, speed };
 }
 
+const formatForecastWeather = (data) => {
+    let { timezone, daily, hourly } = data;
+    daily = daily.slice(1, 7).map(d => {
+        return {
+            title: formatToLocalTime(d.dt, timezone, 'ccc'),
+            temp: d.temp.day,
+            icon: d.weather[0].icon
+        }
+    });
+
+    hourly = hourly.slice(1, 7).map(d => {
+        return {
+            title: formatToLocalTime(d.dt, timezone, 'hh:mm a'),
+            temp: d.temp.day,
+            icon: d.weather[0].icon
+        }
+    });
+
+    return { timezone, daily, hourly };
+};
+
 const getFormattedWeatherData = async (searchParams) => {
     const formattedCurrentWeather = await getWeatherData('weather', searchParams).then(formatCurrentWeather);
-    
-    return formattedCurrentWeather;
+
+    const { lat, lon } = formattedCurrentWeather
+
+    const formattedForecastWeather = await getWeatherData('onecall', {
+        lat,
+        lon,
+        exclude: 'current,minutely,alerts',
+        units: searchParams.units,
+    }).then(formatForecastWeather)
+
+    return { ...formattedCurrentWeather, ...formattedForecastWeather };
 };
+
+const formatToLocalTime = (secs, zone, format = "cccc, dd LLL yyy' | Local time: 'hh:mm a") => DateTime.fromSeconds(secs).setZone(zone).toFormat(format);
 
 export default getFormattedWeatherData
 
